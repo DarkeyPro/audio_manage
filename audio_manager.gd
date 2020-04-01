aae10d347e812acfa94efc9d6ea5aee03b12c108
Darkey <darkey_pro@protonmail.com>
2020-04-01 02:48:20 -0400

    performance improvements

--- audio_manager.gd
+++ audio_manager.gd
@@ -1,5 +1,5 @@
-tool
-extends AudioStreamPlayer
+
+extends Node
 
 export (Array,AudioStreamSample) var tracks_list #the state source
 export (Array,Array,AudioStreamSample) var sub_tracks_list #the phase source
@@ -9,11 +9,13 @@ export (String) var path_to_audio
 
 var tracks 
 var sub_tracks
+var main_audio_player
 var sub_audio_player
 
 var current_state =0
 
 func _ready():
+	main_audio_player =get_node("main")
 	sub_audio_player = get_node("Sub")
 	if !auto:
 		tracks = tracks_list 
@@ -32,14 +34,24 @@ func _play(id_state,id_phase = null,phase_only = false ):
 	else: 
 		_change_phase(id_phase)
 
+func tweening(object:Node,property:String,to,duration=0.0,completed =false,trans_type=Tween.TRANS_LINEAR,ease_type =Tween.EASE_IN_OUT,delay =0.0):
+	var tween = Tween.new()
+	add_child(tween)
+	tween.interpolate_property(object,property,object.get(property),to,duration,trans_type,ease_type,delay)
+	tween.start()
+	if completed:
+		yield(tween,"tween_completed")
+	tween.queue_free()
+
 func _stop():
-	$AnimationPlayer.play("end_phase")
-	yield($AnimationPlayer,"animation_finished")
+	# end phase
+	tweening(sub_audio_player,"volume_db",-80,1.0,true)
 	sub_audio_player.stop()
-	$AnimationPlayer.play("next")
-	yield($AnimationPlayer,"animation_finished")
-	stop()
-	$AnimationPlayer.play("play")
+	#start next
+	tweening(main_audio_player,"volume_db",-80,1.0,true)
+	main_audio_player.stop()
+	#play
+	tweening(main_audio_player,"volume_db",0,1.0)
 
 func chick_sound(state):
 	# makes sure that the list is not empty,equals null and find the track
@@ -47,53 +59,40 @@ func chick_sound(state):
 			var i = 0
 			while i<=tracks.size()-1:
 				if state in tracks[i].resource_path and tracks[i] != null :
-					stream = tracks[i]
-					play()
+					main_audio_player.stream = tracks[i]
+					main_audio_player.play()
 					current_state = i
 				i+=1
 
-
-
 func _play_state(state): 
-	if !playing :
+	if !main_audio_player.playing :
 		chick_sound(state)
 	else:
-		$AnimationPlayer.play("next")
-		yield($AnimationPlayer,"animation_finished")
+		tweening(main_audio_player,"volume_db",-80,1.0,true)
 		chick_sound(state)
-		$AnimationPlayer.play("play")
+		tweening(main_audio_player,"volume_db",0,1.0)
 	
 func _change_phase(phase = null):
-	if playing:
+	if main_audio_player.playing:
 		if sub_audio_player.playing:
-			$AnimationPlayer.play("end_phase")
+			tweening(sub_audio_player,"volume_db",-80,1.0)
 			var temp_sub = sub_tracks[current_state]
 			sub_audio_player.stream = temp_sub[phase]
-			sub_audio_player.stream.loop_begin =  get_playback_position()
-			sub_audio_player.play()
+			sub_audio_player.stream.loop_begin =  main_audio_player.get_playback_position()
 			print("l")
-			yield($AnimationPlayer,"animation_finished")
-			$AnimationPlayer.play("start_phase")
+			sub_audio_player.play()
+			tweening(sub_audio_player,"volume_db",0,1.0,true)
 		else:
 			var temp_sub = sub_tracks[current_state]
 			print("s")
 			sub_audio_player.stream = temp_sub[phase]
-			sub_audio_player.stream.loop_begin =  get_playback_position()
+			sub_audio_player.stream.loop_begin =  main_audio_player.get_playback_position()
 			sub_audio_player.play()
 			pass
 	else:
 		return
 
-#to make sure that this node has all the info you need
-func _get_configuration_warning() ->String:
-	var warnings = ""
-	if not tracks:
-		warnings="your audio list size is 0 change it"
-		return warnings
-	for i in tracks:
-		if not i:
-			warnings="add an audio track to use this node"
-	return warnings
+
 
 func set_state_tracks():
 	return file_find(path_to_audio+"/"+"main")
@@ -110,7 +109,7 @@ func file_find(path):
 		dir.list_dir_begin()
 		var file_name = dir.get_next()
 		var first = file_name
-		while (file_name != ""):
+		while (file_name != "" and file_name != "." and file_name != ".."):
 			paths.append(load(path+"/"+file_name))
 			count += 1
 			file_name = dir.get_next()
@@ -118,6 +117,4 @@ func file_find(path):
 				return 
 	else:
 		print("An error occurred when trying to access the path.")
-	paths.erase(paths[paths.size()-1])
-	paths.erase(paths[paths.size()-1])
 	return paths
